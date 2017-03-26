4e16fd54e50f968019fe11e0a8221d2704734da2
MPW <mpw@0x10,de>
2017-03-26 01:51:00 +0100

    some small improvements

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# todo check for root
-# todo loop over requirements,txt for installation
+# TODO check for root
+# TODO loop over requirements,txt for installation
 
 ######
 # make general symlink
@@ -19,20 +19,23 @@ HAPD=/etc/default/hostapd
 echo -e "\n# added by guestwlan setup" | sudo tee -a $HAPD
 cat config-files/etc-default-hostapd-append | sudo tee -a $HAPD
 echo "# END guestwlan setup" | sudo tee -a $HAPD
+cp config-files/etc-hostapd-hostapd.conf.template config-files/etc-hostapd-hostapd.conf
 sudo ln -s /var/guestwlan/config-files/etc-hostapd-hostapd.conf /etc/hostapd/hostapd.conf
+# TODO ask for custom SSID and change it in hostapd.conf
 
 ######
 # setup dnsmasq
 ######
 
-sudo pt-get install dnsmasq
+sudo apt-get install dnsmasq
 sudo ln -s /var/guestwlan/config-files/etc-dnsmasq.d-guestwlan /etc/dnsmasq.d/guestwlan
 
 ######
 # setup wlan interface
 ######
 
-sudo ln -s /var/guestwlan/config-files/etc-network-interfaces.d-guestwlan /etc/network/interfaces.d/guestwlan
+# sudo ln -s /var/guestwlan/config-files/etc-network-interfaces.d-guestwlan /etc/network/interfaces.d/guestwlan
+cat config-files/etc-network-interfaces.d-guestwlan | sudo tee -a /etc/network/interfaces
 
 ######
 # setup ipv4 forward
