23feb00703dbc36403ab06fac0bbcca784f1256a
MPW <mpw@0x10,de>
2017-03-26 05:32:06 +0200

    MOD: use symlinks wherever possible

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+VARSYMLINK=/var/guestwlan
+VARCONF=$VARSYMLINK/config-files
+DHAPD=/etc/default/hostapd
+
 # TODO check for root
 # TODO loop over requirements,txt for installation
 
@@ -7,20 +11,26 @@
 # make general symlink
 ######
 
-sudo ln -s /home/pi/guestwlan /var/guestwlan
+sudo ln -s /home/pi/guestwlan $VARSYMLINK
+
+######
+# setup bridge wlan interface
+######
+
+sudo apt-get install bridge-utils
+sudo ln -s $VARCONF/etc-network-interfaces.d-guestwlan /etc/network/interfaces.d/guestwlan
 
 ######
 # setup hostapd
 ######
 
 sudo apt-get install hostapd
-HAPD=/etc/default/hostapd
 
-echo -e "\n# added by guestwlan setup" | sudo tee -a $HAPD
-cat config-files/etc-default-hostapd-append | sudo tee -a $HAPD
-echo "# END guestwlan setup" | sudo tee -a $HAPD
+echo -e "\n# added by guestwlan setup" | sudo tee -a $DHAPD
+cat config-files/etc-default-hostapd-append | sudo tee -a $DHAPD
+echo "# END guestwlan setup" | sudo tee -a $DHAPD
 cp config-files/etc-hostapd-hostapd.conf.template config-files/etc-hostapd-hostapd.conf
-sudo ln -s /var/guestwlan/config-files/etc-hostapd-hostapd.conf /etc/hostapd/hostapd.conf
+sudo ln -s $VARCONF/etc-hostapd-hostapd.conf /etc/hostapd/hostapd.conf
 # TODO ask for custom SSID and change it in hostapd.conf
 
 ######
@@ -28,20 +38,27 @@ sudo ln -s /var/guestwlan/config-files/etc-hostapd-hostapd.conf /etc/hostapd/hos
 ######
 
 sudo apt-get install dnsmasq
-sudo ln -s /var/guestwlan/config-files/etc-dnsmasq.d-guestwlan /etc/dnsmasq.d/guestwlan
-
-######
-# setup wlan interface
-######
-
-# sudo ln -s /var/guestwlan/config-files/etc-network-interfaces.d-guestwlan /etc/network/interfaces.d/guestwlan
-cat config-files/etc-network-interfaces.d-guestwlan | sudo tee -a /etc/network/interfaces
+sudo ln -s $VARCONF/etc-dnsmasq.d-guestwlan /etc/dnsmasq.d/guestwlan
 
 ######
 # setup ipv4 forward
 ######
 
-sudo ln -s /var/guestwlan/config-files/etc-sysctl.d-60-guestwlan.conf /etc/sysctl.d/60-guestwlan.conf
+sudo ln -s $VARCONF/etc-sysctl.d-60-guestwlan.conf /etc/sysctl.d/60-guestwlan.conf
+
+######
+# set iptables rule
+######
+
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo apt-get iptables-persistent
+# dpkg-reconfigure iptables-persistent
+
+######
+# setup keygen symlink
+######
+
+sudo ln -s $VARSYMLINK/wlankeygen /usr/local/bin/wlankeyken
 
 ######
 # make local config
